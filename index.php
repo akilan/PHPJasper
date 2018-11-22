68bd9b70e6233c1baa895ec1c9fce6cdb5960735
Akilan <akilanr@outlook.com>
2018-11-22 12:45:18 +0530

    DB with params feature added

--- index.php
+++ index.php
@@ -7,10 +7,20 @@ $input = __DIR__ . '/jasper/hello_world.jrxml';
 $jasper = new PHPJasper;
 $jasper->compile($input)->execute();
 */
-$input = __DIR__ . '/jasper/hello_world.jasper';  
-$output = __DIR__ . '/jasper/examples';    
+$input = __DIR__ . '/jasper/kgfs_P2P_report.jrxml';  
+$output = __DIR__ . '/jasper/examples';
+
 $options = [ 
-    'format' => ['pdf', 'rtf'] 
+    'format' => ['pdf', 'rtf'],
+    'params' => ['bank_name'=>'Pudhuaaru','kgfs_date'=>'2015-05-28'],
+    'db_connection' => [
+        'driver' => 'mysql', //mysql, ....
+        'username' => '',
+        'password' => '',
+        'host' => 'sit.perdix.co.in',
+        'database' => 'sit_kgfs_financialForms',
+        'port' => '3306'
+    ]
 ];
 
 $jasper = new PHPJasper;
@@ -20,3 +30,4 @@ $jasper->process(
     $output,
     $options
 )->execute();
+echo "Report Generated";
